5e7b2291a1f951b70b47c780a65bb0ba6319c75c
Pavel Lobovski <lobovski.p.bartoli.mo@gmail.com>
2021-10-06 10:54:59 +0200

    Finished Gatsby Tutorial Part 2

---
+++ src/components/my-component.js
@@ -0,0 +1,11 @@
+Copysrc/components/my-component.js: copy code to clipboard
+import * as React from 'react'
+import { title } from './my-component.module.css'
+const MyComponent = () => {
+  return (
+    <h1 className={title}>
+      Super Sweet Title Page
+    </h1>
+  )
+}
+export default MyComponent
